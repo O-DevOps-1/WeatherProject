e3cabc7520f0bfd8aa41ce49016997ac76942a38
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-10 23:13:03 +0200

    Update Tab1Fragment.java

---
+++ app/src/main/java/com/example/weather/Tab1Fragment.java
@@ -0,0 +1,26 @@
+package com.example.weather;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Fragment extends Fragment {
+    private TextView textView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+        textView = view.findViewById(R.id.text_view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Initialize the fragment here
+    }}
