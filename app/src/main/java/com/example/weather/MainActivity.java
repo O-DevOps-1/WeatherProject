979f6574e9a5a6bdd87c27283398e26e9b3d8a21
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-10 23:12:11 +0200

    Update MainActivity.java

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+    }}
