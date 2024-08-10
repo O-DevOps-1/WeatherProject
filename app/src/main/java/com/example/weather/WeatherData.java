44ef72eba6df522c91f48b324831e72fcb921bf8
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-10 23:13:53 +0200

    Update WeatherData.java

---
+++ app/src/main/java/com/example/weather/WeatherData.java
@@ -0,0 +1,17 @@
+package com.example.weather;
+
+public class WeatherData {
+    private String date;
+    private String temperature;
+    private String humidity;
+    private String windSpeed;
+
+    public WeatherData(String date, String temperature, String humidity, String windSpeed) {
+        this.date = date;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+    }
+
+    // getters and setters
+  }
